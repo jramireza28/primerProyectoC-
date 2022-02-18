4bd0766bc4b5d496cc77c00c4df535dfb093ff40
jramireza28 <jramireza28@miumg.edu.gt>
2022-02-17 23:28:12 -0600

    #creacion de primer ejercicio:jairo ramirez

--- proyecto 1 de progra 1/Program.cs
+++ proyecto 1 de progra 1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public class aprendiendo
+{
+    static void Main(string[] args)
+    {
+
+
+        byte edad;
+        Console.WriteLine("ingrese su edad");
+        edad = Convert.ToByte(Console.ReadLine());
+        Console.WriteLine("usted no aparenta " + edad + "años");
+        Console.WriteLine();
+
+
+
+    }
+
+
+}

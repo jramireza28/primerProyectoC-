c9d700f5e0e600f35991a8a018e7a08de8cc84ff
jramireza28 <jramireza28@miumg.edu.gt>
2022-02-17 23:32:38 -0600

    #creacion del segundo ejercicio:jairo ramirez

--- proyecto 1 de progra 1/Program.cs
+++ proyecto 1 de progra 1/Program.cs
@@ -15,7 +15,15 @@ public class aprendiendo
         Console.WriteLine("usted no aparenta " + edad + "años");
         Console.WriteLine();
 
-
+        byte num1, num2;
+        ushort mult;
+        Console.WriteLine("ingrese un numero");
+        num1 = Convert.ToByte(Console.ReadLine());
+        Console.WriteLine("ingrese un numero");
+        num2 = Convert.ToByte(Console.ReadLine());
+        mult = (ushort)(num1 * num2);
+        Console.WriteLine("resultado de la multiplicacion: " + mult);
+        Console.WriteLine();
 
     }
 
